634ca884d36d16ad6aa34e3e0c30e790deb50dc5
irpani <shaikirfanoptum@gmail.com>
2020-09-24 17:45:44 +0530

    Bye

---
+++ src/Test.java
@@ -0,0 +1,23 @@
+
+class C {
+	int x;
+	int y;
+	int z;
+
+	public int add(int x, int y) {
+
+		return z = x / y;
+
+	}
+
+}
+
+public class Test {
+
+	public static void main(String[] args) {
+		C obj = new C();
+		System.out.println(obj.add(10, 20));
+
+	}
+
+}
